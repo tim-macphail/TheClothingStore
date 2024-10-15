fd447d61c1b8b6af205bf98ca1123377fc023520
tim-macphail <timothy.macphail@gmail.com>
2024-10-15 14:07:43 -0600

    conditional render Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+
+
+const AppContent = () => {
+  const location = useLocation();
+  const showNavbar = location.pathname !== '/cart';
+
+  return (
+    <div>
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+        </ul>
+    </nav>
+);
+
+export default Navbar
